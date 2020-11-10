945e5769c68873f687659b07fc56f9c75abd3c42
EC2 Default User <ec2-user@ip-172-31-18-55.us-east-2.compute.internal>
2020-11-10 01:33:05 +0000

    base files and images

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8"/>
+        <title> Justin Thomas: Personal Website</title>
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1> Justin Thomas </h1>
+        </header>
+        <hr/>
+        <nav>
+            <a href="index.php" class="current-page">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+        <br/>
+        <main>
+            <figure>
+                <img src="img/WebsiteHeadshot.jpg" alt="Justin Thomas Profile Picture"/>
+            </figure>
+            
+            <div>
+                Hello!<br/>
+                <p>Thank you for visiting my professional portfolio website.</p>
+                <p>I'm a technical program manager</p>
+                <p>Feel free to contact me!</p>
+                <br/>
+                <em>"Hard work, <strong>dedication.</strong> - Floyd Mayweather</em>
+                <br/>
+            </div>
+            
+        </main>
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            CST336 Internet Programming. 2020&copy; Thomas <br/>
+            <strong>Disclaimer:</strong> The information in this page is fictitious.
+            <br/>
+            It is used for academic purposes only.
+            <figure>
+                <img src="img/csumbLogoWhite.png"/>
+            </figure>
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
